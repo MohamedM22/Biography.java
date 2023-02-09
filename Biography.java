47c4a0e0fd869a844ca52cf16d2081e223550b76
Mohamed Moulaye <109633435+MohamedM22@users.noreply.github.com>
2023-02-09 14:02:29 -0500

    Add files via upload

---
+++ Biography.java
@@ -0,0 +1,23 @@
+public class Biography {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+
+        String name = "Mohamed";
+        int age = 25;
+        String country = "Niger";
+        String hobby = "Reading";
+        int hours = 2;
+        String game = "Fifa";
+        String subject = "Geography";
+        char grade = 'B';
+     
+        System.out.println("My name is "+ name +". I'm " +age+ " years old, and I'm from " +country+".");
+        System.out.println("My favourite hobby is "+hobby+". I read for "+hours+" hours a day.");
+        System.out.println("When I'm tired, I like to play " +game+".");
+        System.out.println("In school, my favourite subject was " + subject +", I scored a " + grade+".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
